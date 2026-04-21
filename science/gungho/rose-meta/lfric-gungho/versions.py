fc3bbadefa7689c476a4b42f48826e7a77c7abaf
Samantha Pullen <samantha.pullen@metoffice.gov.uk>
2026-04-21 12:07:19 +0100

    Apply upgrade macro vn3.1_t443

--- science/gungho/rose-meta/lfric-gungho/versions.py
+++ science/gungho/rose-meta/lfric-gungho/versions.py
@@ -20,24 +20,24 @@ class UpgradeError(Exception):
 
 """
 Copy this template and complete to add your macro
-
 class vnXX_txxx(MacroUpgrade):
     # Upgrade macro for <TICKET> by <Author>
-
     BEFORE_TAG = "vnX.X"
     AFTER_TAG = "vnX.X_txxx"
-
     def upgrade(self, config, meta_config=None):
         # Add settings
         return config, self.reports
 """
+
+
 class vn31_t443(MacroUpgrade):
-    # Upgrade macro for PR #443 by Samantha Pullen
+    """Upgrade macro for ticket #443 by Samantha Pullen."""
 
     BEFORE_TAG = "vn3.1"
     AFTER_TAG = "vn3.1_t443"
 
     def upgrade(self, config, meta_config=None):
+        # Commands From: rose-meta/lfric-gungho
         # Add name entry to iau_addinf_io namelist
         self.add_setting(
             config, ["namelist:iau_addinf_io(addinf1)", "name"], "''"
@@ -46,14 +46,11 @@ class vn31_t443(MacroUpgrade):
             config, ["namelist:iau_addinf_io(addinf2)", "name"], "''"
         )
         # Add name entry to iau_ainc_io namelist
-        self.add_setting(
-            config, ["namelist:iau_ainc_io(ainc1)", "name"], "''"
-        )
-        self.add_setting(
-            config, ["namelist:iau_ainc_io(ainc2)", "name"], "''"
-        )
+        self.add_setting(config, ["namelist:iau_ainc_io(ainc1)", "name"], "''")
+        self.add_setting(config, ["namelist:iau_ainc_io(ainc2)", "name"], "''")
         # Add name entry to iau_bcorr_io namelist
         self.add_setting(
             config, ["namelist:iau_bcorr_io(bcorr1)", "name"], "''"
         )
+
         return config, self.reports
